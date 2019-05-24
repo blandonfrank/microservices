7e3e0b0b0e2b4d357bedb941bd45045f3380fa26
Frank Blandon <blandonfrank@gmail.com>
2019-05-24 11:29:18 -0600

    Add jaxb to Eurekas discovery server as a dependency. It wouldn't start without it using jdk 11

--- transactions-service/src/main/java/com/redpony/transactionsservice/configuration/TransactionConfiguration.java
+++ transactions-service/src/main/java/com/redpony/transactionsservice/configuration/TransactionConfiguration.java
@@ -1,14 +1,11 @@
 package com.redpony.transactionsservice.configuration;
 
 import com.redpony.transactionsservice.service.PortfolioEventConsumer;
-import com.redpony.transactionsservice.service.TransactionService;
 import io.eventuate.jdbckafka.TramJdbcKafkaConfiguration;
-import io.eventuate.tram.events.publisher.DomainEventPublisher;
 import io.eventuate.tram.events.publisher.TramEventsPublisherConfiguration;
 import io.eventuate.tram.events.subscriber.DomainEventDispatcher;
 import io.eventuate.tram.messaging.consumer.MessageConsumer;
 import io.eventuate.tram.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
